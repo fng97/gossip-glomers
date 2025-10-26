d7ce9dcd95b87c4f8e42361591bb0efe8cd3eacb
fng97 <fng97@icloud.com>
2025-10-26 17:07:43 +0000

    Add `zig build check` for ZLS

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "gossip_glomers",
         .root_module = b.createModule(.{
@@ -22,4 +23,11 @@ pub fn build(b: *std.Build) void {
     const exe_tests = b.addTest(.{ .root_module = exe.root_module });
     const run_exe_tests = b.addRunArtifact(exe_tests);
     test_step.dependOn(&run_exe_tests.step);
+
+    // See https://zigtools.org/zls/guides/build-on-save/.
+    const check_step = b.step("check", "Check if everything compiles");
+    const check_exe = b.addExecutable(.{ .name = "check", .root_module = exe.root_module });
+    const check_tests = b.addTest(.{ .root_module = exe.root_module });
+    check_step.dependOn(&check_exe.step);
+    check_step.dependOn(&check_tests.step);
 }
