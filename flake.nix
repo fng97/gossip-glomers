fd512503ede55969787d84b8c4941e51584bb7ab
fng97 <fng97@icloud.com>
2025-10-26 16:51:35 +0000

    Add Nix flake with Zig 0.15.2 and ZLS

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    zls-overlay.url = "github:zigtools/zls?ref=0.15.0";
+    zls-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, zig-overlay, zls-overlay, ... }:
+    let
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems (system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [
+                zig-overlay.overlays.default
+                (final: prev: {
+                  zig = final.zigpkgs."0.15.2";
+                  zls = zls-overlay.packages.${system}.default;
+                })
+              ];
+            };
+          });
+    in {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell { packages = with pkgs; [ zig zls ]; };
+      });
+    };
+}
