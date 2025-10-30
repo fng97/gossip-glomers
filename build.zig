e2c6f4239a473b3960115e38a60d4423370beefe
fng97 <git@francisco.wiki>
2025-10-30 20:11:58 +0000

    Run maelstrom tests from zig build test

--- build.zig
+++ build.zig
@@ -17,12 +17,30 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
-    run_cmd.step.dependOn(b.getInstallStep());
 
     const test_step = b.step("test", "Run tests");
     const exe_tests = b.addTest(.{ .root_module = exe.root_module });
     const run_exe_tests = b.addRunArtifact(exe_tests);
     test_step.dependOn(&run_exe_tests.step);
+    const echo = b.addSystemCommand(&.{
+        "maelstrom",    "test",
+        "--workload",   "echo",
+        "--node-count", "1",
+        "--time-limit", "10",
+    });
+    echo.addPrefixedArtifactArg("--bin=", exe);
+    test_step.dependOn(&echo.step);
+    const unique_ids = b.addSystemCommand(&.{
+        "maelstrom",      "test",
+        "--workload",     "unique-ids",
+        "--rate",         "1000",
+        "--node-count",   "3",
+        "--time-limit",   "30",
+        "--availability", "total",
+        "--nemesis",      "partition",
+    });
+    unique_ids.addPrefixedArtifactArg("--bin=", exe);
+    test_step.dependOn(&unique_ids.step);
 
     // See https://zigtools.org/zls/guides/build-on-save/.
     const check_step = b.step("check", "Check if everything compiles");
